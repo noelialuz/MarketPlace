fad3cad8819798663a6f97f0ca00c948a7f135c8
“noelialuz” <noelialuzfernandez@gmail.com>
2026-06-23 18:38:19 -0300

    Add new functions and test

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Idenfitier: MIT
+// SPDX-License-Identifier: MIT 
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.35;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
@@ -14,19 +14,53 @@ contract NFTMarketplace is ReentrancyGuard {
         uint256 price;
     }
 
+    address public owner;
+    address public feeAccount; 
+    uint256 public feePercent; 
+
     mapping(address => mapping(uint256 => Listing)) public listing;
 
     event NFTListed(address indexed seller, address indexed nftAddress, uint256 indexed tokenId, uint256 price);
     event NFTCancelled(address indexed seller, address indexed nftAddress, uint256 indexed tokenId);
     event NFTSold(address indexed buyer, address indexed seller, address indexed nftAddress, uint256 tokenId, uint256 price);
+    event NFTPriceUpdated(address indexed seller, address indexed nftAddress, uint256 indexed tokenId, uint256 newPrice);
+    event FeeAccountChanged(address indexed oldAccount, address indexed newAccount);
+    event FeePercentChanged(uint256 oldPercent, uint256 newPercent);
 
-    constructor() {}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
 
-    // List NFTs
+    constructor(address feeAccount_, uint256 feePercent_) {
+        owner = msg.sender;
+        require(feeAccount_ != address(0), "Invalid fee account");
+        require(feePercent_ <= 1000, "Fee cannot exceed 10%"); 
+        feeAccount = feeAccount_;
+        feePercent = feePercent_;
+    }
+
+    function setFeeAccount(address feeAccount_) external onlyOwner {
+        require(feeAccount_ != address(0), "Invalid address");
+        emit FeeAccountChanged(feeAccount, feeAccount_);
+        feeAccount = feeAccount_;
+    }
+
+    function setFeePercent(uint256 feePercent_) external onlyOwner {
+        require(feePercent_ <= 1000, "Fee cannot exceed 10%");
+        emit FeePercentChanged(feePercent, feePercent_);
+        feePercent = feePercent_;
+    }
+
     function listNFT(address nftAddress_, uint256 tokenId_, uint256 price_) external nonReentrant {
         require(price_ > 0, "Price can not be 0");
         address owner_ = IERC721(nftAddress_).ownerOf(tokenId_);
         require(owner_ == msg.sender, "You are not the owner of the NFT");
+        require(
+            IERC721(nftAddress_).getApproved(tokenId_) == address(this) || 
+            IERC721(nftAddress_).isApprovedForAll(msg.sender, address(this)), 
+            "Marketplace not approved"
+        );
 
         Listing memory listing_ = Listing({
             seller: msg.sender,
@@ -40,8 +74,6 @@ contract NFTMarketplace is ReentrancyGuard {
         emit NFTListed(msg.sender, nftAddress_, tokenId_, price_);
     }
 
-
-    // Buy NFTs
     function buyNFT(address nftAddress_, uint256 tokenId_) external payable nonReentrant {
         Listing memory listing_ = listing[nftAddress_][tokenId_];
         require(listing_.price > 0, "Listing not exists");
@@ -49,16 +81,22 @@ contract NFTMarketplace is ReentrancyGuard {
 
         delete listing[nftAddress_][tokenId_];
 
+        uint256 feeAmount = (msg.value * feePercent) / 10000;
+        uint256 sellerAmount = msg.value - feeAmount;
+
         IERC721(nftAddress_).safeTransferFrom(listing_.seller, msg.sender, listing_.tokenId);
 
-        (bool success, ) = listing_.seller.call{value: msg.value}("");
-        require(success, "Fail");
+        if (feeAmount > 0) {
+            (bool successFee, ) = feeAccount.call{value: feeAmount}("");
+            require(successFee, "Fee transfer failed");
+        }
+
+        (bool successSeller, ) = listing_.seller.call{value: sellerAmount}("");
+        require(successSeller, "Seller transfer failed");
         
         emit NFTSold(msg.sender, listing_.seller, listing_.nftAddress, listing_.tokenId, listing_.price);
     }
 
-
-    // Cancel List
     function cancelList(address nftAddress_, uint256 tokenId_) external nonReentrant {
         Listing memory listing_ = listing[nftAddress_][tokenId_];
         require(listing_.seller == msg.sender, "You are not the listing owner");
@@ -66,5 +104,32 @@ contract NFTMarketplace is ReentrancyGuard {
         delete listing[nftAddress_][tokenId_];
 
         emit NFTCancelled(msg.sender, nftAddress_, tokenId_);
+    }
+
+    function updatePrice(address nftAddress_, uint256 tokenId_, uint256 newPrice_) external nonReentrant {
+        require(newPrice_ > 0, "Price can not be 0");
+        Listing storage listing_ = listing[nftAddress_][tokenId_];
+        require(listing_.seller == msg.sender, "You are not the listing owner");
+
+        listing_.price = newPrice_;
+
+        emit NFTPriceUpdated(msg.sender, nftAddress_, tokenId_, newPrice_);
+    }
+
+    function removeStaleListing(address nftAddress_, uint256 tokenId_) external nonReentrant {
+        Listing memory listing_ = listing[nftAddress_][tokenId_];
+        require(listing_.price > 0, "Listing does not exist");
+
+        address currentOwner = IERC721(nftAddress_).ownerOf(tokenId_);
+        
+        bool isNoLongerOwner = currentOwner != listing_.seller;
+        bool isNotApproved = IERC721(nftAddress_).getApproved(tokenId_) != address(this) && 
+                             !IERC721(nftAddress_).isApprovedForAll(listing_.seller, address(this));
+
+        require(isNoLongerOwner || isNotApproved, "Listing is still valid and active");
+
+        delete listing[nftAddress_][tokenId_];
+        
+        emit NFTCancelled(listing_.seller, nftAddress_, tokenId_);
     }
 }
--- test/NFTMarketplace.t.sol
+++ test/NFTMarketplace.t.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.24;
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.35;
 
 import "forge-std/Test.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
@@ -18,11 +19,13 @@ contract NFTMarketplaceTest is Test {
     MockNFT nft;
     address deployer = vm.addr(1); 
     address user = vm.addr(2);
+    address feeAccount = vm.addr(4);
+    uint256 feePercent = 250;
     uint256 tokenId = 0;
 
     function setUp() public {
         vm.startPrank(deployer);
-        marketplace = new NFTMarketplace();
+        marketplace = new NFTMarketplace(feeAccount, feePercent);
         nft = new MockNFT();
         vm.stopPrank();
 
@@ -58,8 +61,18 @@ contract NFTMarketplaceTest is Test {
         vm.stopPrank();
     }
 
+    function testShouldRevertIfNotApproved() public {
+        vm.startPrank(user);
+        
+        vm.expectRevert("Marketplace not approved");
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+        
+        vm.stopPrank();
+    }
+
     function testListNFTCorreclty() public {
         vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
 
         (address sellerBefore,,,)= marketplace.listing(address(nft), tokenId);
         marketplace.listNFT(address(nft), tokenId, 1e18);
@@ -72,6 +85,7 @@ contract NFTMarketplaceTest is Test {
 
     function testListShouldRevertIfNotOwner() public {
         vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
 
         (address sellerBefore,,,)= marketplace.listing(address(nft), tokenId);
         marketplace.listNFT(address(nft), tokenId, 1e18);
@@ -91,6 +105,7 @@ contract NFTMarketplaceTest is Test {
 
     function testCancelListShouldWorkCorrectly() public {
         vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
 
         (address sellerBefore,,,)= marketplace.listing(address(nft), tokenId);
         marketplace.listNFT(address(nft), tokenId, 1e18);
@@ -117,6 +132,7 @@ contract NFTMarketplaceTest is Test {
 
     function testCanNotBuyWithIncorrectPay() public {
         vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
 
         uint256 price = 1e18;
         (address sellerBefore,,,)= marketplace.listing(address(nft), tokenId);
@@ -137,17 +153,12 @@ contract NFTMarketplaceTest is Test {
         vm.stopPrank();
     }
 
-    function testShouldBuyNFTCorrectly() public {
+    function testShouldBuyNFTCorrectlyWithFees() public {
         vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
 
         uint256 price = 1e18;
-        (address sellerBefore,,,)= marketplace.listing(address(nft), tokenId);
         marketplace.listNFT(address(nft), tokenId, price);
-        (address sellerAfter,,,)= marketplace.listing(address(nft), tokenId);
-
-        assert(sellerBefore == address(0) && sellerAfter == user);
-
-        nft.approve(address(marketplace), tokenId);
         vm.stopPrank();
 
         address user2 = vm.addr(3);
@@ -155,17 +166,196 @@ contract NFTMarketplaceTest is Test {
         vm.deal(user2, price);
         
         uint256 balanceBefore = address(user).balance;
-        address ownerBefore = nft.ownerOf(tokenId);
-        (address sellerBefore2,,,)= marketplace.listing(address(nft), tokenId);
+        uint256 feeBalanceBefore = address(feeAccount).balance;
+        
         marketplace.buyNFT{value: price}(address(nft), tokenId);
-        (address sellerAfter2,,,)= marketplace.listing(address(nft), tokenId);
-        address ownerAfter = nft.ownerOf(tokenId);
-        uint256 balanceAfter = address(user).balance;
+        
+        uint256 expectedFee = (price * feePercent) / 10000;
+        uint256 expectedSellerAmount = price - expectedFee;
 
-        assert(sellerBefore2 == user && sellerAfter2 == address(0));
-        assert(ownerBefore == user && ownerAfter == user2);
-        assert(balanceAfter == balanceBefore + price);
+        assert(nft.ownerOf(tokenId) == user2);
+        assert(address(user).balance == balanceBefore + expectedSellerAmount);
+        assert(address(feeAccount).balance == feeBalanceBefore + expectedFee);
       
         vm.stopPrank();
+    }
+
+    function testUpdatePriceCorrectly() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+
+        uint256 newPrice = 2e18;
+        marketplace.updatePrice(address(nft), tokenId, newPrice);
+
+        (,,,uint256 priceAfter) = marketplace.listing(address(nft), tokenId);
+        assert(priceAfter == newPrice);
+        vm.stopPrank();
+    }
+
+    function testUpdatePriceShouldRevertIfPriceIsZero() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+
+        vm.expectRevert("Price can not be 0");
+        marketplace.updatePrice(address(nft), tokenId, 0);
+        vm.stopPrank();
+    }
+
+    // New tests
+    function testUpdatePriceShouldRevertIfNotOwner() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.expectRevert("You are not the listing owner");
+        marketplace.updatePrice(address(nft), tokenId, 2e18);
+        vm.stopPrank();
+    }
+
+    function testConstructorRevertsIfFeeAccountIsZero() public {
+        vm.startPrank(deployer);
+        vm.expectRevert("Invalid fee account");
+        new NFTMarketplace(address(0), 250);
+        vm.stopPrank();
+    }
+
+    function testConstructorRevertsIfFeePercentExceedsLimit() public {
+        vm.startPrank(deployer);
+        vm.expectRevert("Fee cannot exceed 10%");
+        new NFTMarketplace(feeAccount, 1001);
+        vm.stopPrank();
+    }
+
+    function testSetFeeAccountCorrectly() public {
+        vm.startPrank(deployer);
+        address newFeeAccount = vm.addr(5);
+        marketplace.setFeeAccount(newFeeAccount);
+        assert(marketplace.feeAccount() == newFeeAccount);
+        vm.stopPrank();
+    }
+
+    function testSetFeeAccountRevertsIfNotOwner() public {
+        vm.startPrank(user);
+        vm.expectRevert("Not the contract owner");
+        marketplace.setFeeAccount(vm.addr(5));
+        vm.stopPrank();
+    }
+
+    function testSetFeeAccountRevertsIfZeroAddress() public {
+        vm.startPrank(deployer);
+        vm.expectRevert("Invalid address");
+        marketplace.setFeeAccount(address(0));
+        vm.stopPrank();
+    }
+
+    function testSetFeePercentCorrectly() public {
+        vm.startPrank(deployer);
+        uint256 newFeePercent = 500;
+        marketplace.setFeePercent(newFeePercent);
+        assert(marketplace.feePercent() == newFeePercent);
+        vm.stopPrank();
+    }
+
+    function testSetFeePercentRevertsIfNotOwner() public {
+        vm.startPrank(user);
+        vm.expectRevert("Not the contract owner");
+        marketplace.setFeePercent(500);
+        vm.stopPrank();
+    }
+
+    function testSetFeePercentRevertsIfExceedsLimit() public {
+        vm.startPrank(deployer);
+        vm.expectRevert("Fee cannot exceed 10%");
+        marketplace.setFeePercent(1001);
+        vm.stopPrank();
+    }
+
+    function testRemoveStaleListingIfOwnerChanged() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+        
+        address user2 = vm.addr(3);
+        nft.transferFrom(user, user2, tokenId);
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        marketplace.removeStaleListing(address(nft), tokenId);
+        (address seller,,, ) = marketplace.listing(address(nft), tokenId);
+        assert(seller == address(0));
+        vm.stopPrank();
+    }
+
+    function testRemoveStaleListingIfApprovalRemoved() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+        
+        nft.approve(address(0), tokenId);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        marketplace.removeStaleListing(address(nft), tokenId);
+        (address seller,,, ) = marketplace.listing(address(nft), tokenId);
+        assert(seller == address(0));
+        vm.stopPrank();
+    }
+
+    function testRemoveStaleListingRevertsIfListingDoesNotExist() public {
+        vm.startPrank(user);
+        vm.expectRevert("Listing does not exist");
+        marketplace.removeStaleListing(address(nft), tokenId);
+        vm.stopPrank();
+    }
+
+    function testRemoveStaleListingRevertsIfListingIsStillValid() public {
+        vm.startPrank(user);
+        nft.approve(address(marketplace), tokenId);
+        marketplace.listNFT(address(nft), tokenId, 1e18);
+        vm.stopPrank();
+
+        address user2 = vm.addr(3);
+        vm.startPrank(user2);
+        vm.expectRevert("Listing is still valid and active");
+        marketplace.removeStaleListing(address(nft), tokenId);
+        vm.stopPrank();
+    }
+
+    function testListNFTWithApprovalForAll() public {
+    vm.startPrank(user);
+    nft.setApprovalForAll(address(marketplace), true);
+
+    marketplace.listNFT(address(nft), tokenId, 1e18);
+    (address seller,,,) = marketplace.listing(address(nft), tokenId);
+    
+    assert(seller == user);
+    vm.stopPrank();
+}
+
+function testRemoveStaleListingWhenOwnerChangedAndApprovalRemoved() public {
+    vm.startPrank(user);
+    nft.approve(address(marketplace), tokenId);
+    marketplace.listNFT(address(nft), tokenId, 1e18);
+    
+    address user2 = vm.addr(3);
+    nft.transferFrom(user, user2, tokenId);
+    vm.stopPrank();
+
+    vm.startPrank(user2);
+    nft.approve(address(0), tokenId);
+    vm.stopPrank();
+
+    address user3 = vm.addr(5);
+    vm.startPrank(user3);
+    marketplace.removeStaleListing(address(nft), tokenId);
+    (address seller,,,) = marketplace.listing(address(nft), tokenId);
+    assert(seller == address(0));
+    vm.stopPrank();
     }
 }
